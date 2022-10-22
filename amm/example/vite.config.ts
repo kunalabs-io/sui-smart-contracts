db4467211be4542ebed44ee736482e74d2c9fc44
Krešimir Klas <github@kklas.net>
2022-10-22 09:29:17 +0000

    change vite dev host config env var to DEV_HOST

--- amm/example/vite.config.ts
+++ amm/example/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: process.env['VITE_DEV_HOST'],
+    host: process.env['DEV_HOST'],
   },
   build: {
     target: 'ES2020',
