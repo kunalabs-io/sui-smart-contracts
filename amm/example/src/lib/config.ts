17312bd24086c668cfd5f244aaaaf07ef44214d3
Krešimir Klas <github@kklas.net>
2022-10-28 07:41:20 +0000

    [amm example] update pool package id

--- amm/example/src/lib/config.ts
+++ amm/example/src/lib/config.ts
@@ -6,6 +6,6 @@ export interface Config {
 
 export const CONFIG: Config = {
   rpcUrl: 'https://fullnode.devnet.sui.io:443',
-  ammPackageId: '0x12e1d63c87a36e3be3d6b85f7c2975e2a89684aa',
+  ammPackageId: '0x9cfdd454975b82d30e3d2f3ddc5fcbc3baa1be7e',
   ammDefaultPools: [],
 }
