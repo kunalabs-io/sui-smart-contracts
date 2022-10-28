8e4b1a36b5d1431bd0e9de4fc04ce748b726af5b
Krešimir Klas <github@kklas.net>
2022-10-28 07:36:54 +0000

    [amm example] remove example coin from config

--- amm/example/src/lib/config.ts
+++ amm/example/src/lib/config.ts
@@ -2,14 +2,10 @@ export interface Config {
   readonly rpcUrl: string
   readonly ammPackageId: string
   readonly ammDefaultPools: string[]
-  readonly exampleCoinPackageId: string
 }
 
 export const CONFIG: Config = {
   rpcUrl: 'https://fullnode.devnet.sui.io:443',
-
   ammPackageId: '0x12e1d63c87a36e3be3d6b85f7c2975e2a89684aa',
-  exampleCoinPackageId: '0x0ae02752bcdc66dc9f2393a73b3ade083bc4df71',
-
   ammDefaultPools: [],
 }
