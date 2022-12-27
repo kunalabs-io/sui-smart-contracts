a88612ae12a1834d3422ede66558017bd7e71a68
Krešimir Klas <github@kklas.net>
2022-12-27 21:54:14 +0000

    [amm] redeploy to devnet 0.19

--- amm/example-ui/src/lib/config.ts
+++ amm/example-ui/src/lib/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   rpcUrl: 'https://fullnode.devnet.sui.io:443',
-  ammPackageId: '0x5a4cff4897fef7722a953f6b3f458d5b98afaf2e',
-  ammPoolRegistryObj: '0x601f51e99c19ea8f4b708d4040436523271b2ff7',
+  ammPackageId: '0x89cd11921416f5fb2e3e5e777a8ca4cdf53de218',
+  ammPoolRegistryObj: '0x05f6db8b712609f4bc6a5f774a3f33a95bfbf2f6',
   ammDefaultPools: [],
   defaultSlippagePct: 1,
   fetchPoolsViaEvents: true,
