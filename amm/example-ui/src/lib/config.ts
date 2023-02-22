7d7d623527032f7fcac8f2a05027ab0851ab295e
Krešimir Klas <github@kklas.net>
2023-02-22 22:09:06 +0000

    [amm] publish to devnet 0.27.0

--- amm/example-ui/src/lib/config.ts
+++ amm/example-ui/src/lib/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   rpcUrl: 'https://fullnode.devnet.sui.io:443',
-  ammPackageId: '0x36eb3f3d8ca084bb43f077222b481836173616e9',
-  ammPoolRegistryObj: '0x44d3a3a930f1a063b994c6ae9c4d192c3069ba46',
+  ammPackageId: '0x2734cdf57bfadeba0059135e9fca1a3fe9456940',
+  ammPoolRegistryObj: '0x40679eb92c06fb9512ef0618d28a9c9e835019a5',
   ammDefaultPools: [],
   defaultSlippagePct: 1,
   fetchPoolsViaEvents: true,
